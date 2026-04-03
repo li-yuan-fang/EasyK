77f95b1bf48c991a355e52c2318f8f1bf80fabaf
li_yuan_fang <minecraft6@126.com>
2026-04-03 16:27:33 +0800

    性能优化适配低性能设备

--- K/CefSetting.vb
+++ K/CefSetting.vb
@@ -4,6 +4,8 @@ Imports CefSharp
 Public Class CefSetting
     Inherits CefSettingsBase
 
+    Private Const UA As String = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36"
+
     ''' <summary>
     ''' 初始化
     ''' </summary>
@@ -20,11 +22,25 @@ Public Class CefSetting
         '默认语言
         Locale = "zh-CN"
 
-        '音频托管
-        If Settings.Settings.Audio.IsDummyAudio Then CefCommandLineArgs.Add("disable-audio-output", "1")
+        'UserAgent
+        UserAgent = UA
 
-        '允许自动播放
-        CefCommandLineArgs.Add("autoplay-policy", "no-user-gesture-required")
+        '命令行参数
+        With CefCommandLineArgs
+
+            '音频托管
+            If Settings.Settings.Audio.IsDummyAudio Then .Add("disable-audio-output", "1")
+
+            '允许自动播放
+            .Add("autoplay-policy", "no-user-gesture-required")
+
+            'GPU加速
+            .Add("enable-gpu", "1")
+            .Add("enable-gpu-compositing", "1")
+            .Add("ignore-gpu-blocklist", "1")
+            .Add("use-angle", "d3d11")
+            .Add("enable-accelerated-video-decode", "1")
+        End With
 
         '日志
         If Settings.Settings.DebugMode Then
